8c299f44e4a615954491b52e1a030ad6616f44b3
SayKham99 <x5292621@gmail.com>
2022-11-15 22:47:34 +0500

    "feat:some changes"

---
+++ src/languages/index.js
@@ -0,0 +1,11 @@
+import ru from './ru.json';
+import en from './en.json';
+import uz from './uz.json';
+
+export const dictionaryList = {en, ru, uz};
+
+export const languageOptions = {
+    en: 'English',
+    ru: 'Russian',
+    uz: 'Uzbek'
+};
